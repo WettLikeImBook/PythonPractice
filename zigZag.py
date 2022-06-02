c64ed2715f4206570cffe80152ca6dbbf9744f6f
gyyost <yostgrant@icloud.com>
2022-06-01 20:08:02 -0400

    asdf

--- zigZag.py
+++ zigZag.py
@@ -5,9 +5,9 @@ indentIncreasing = True # Whether the indentation is increasing or not
 
 try:
     while True: # Main program loop
-        print(' ' * indent, end='')
+        print('        ' * indent, end='')
         print('*******')
-        time.sleep(.1) # Pause for 1/10 of a second
+        time.sleep(.15) # Pause for 1/10 of a second
 
         if indentIncreasing:
             # Increase the number of spaces
