6a7bbd0f4e8a1b9638e829c0ad188354bde0c719
gyyost <yostgrant@icloud.com>
2022-06-06 16:44:17 -0400

    pass

---
+++ passGenerator.py
@@ -0,0 +1,19 @@
+# Password generator
+import random
+
+lowerAlphaList = ('abcdefghijklmnopqrstuvwxyz')
+upperAlphaList = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+specialList = ('!@#$%^&*()-_<>?')
+digitList = ('0123456789')
+newPass = ''
+
+allChar = lowerAlphaList + upperAlphaList + specialList + digitList
+
+print('How long do you want the password to be? (8-64): ')
+choice = input()
+passLength = int(choice)
+
+for i in range(passLength):
+    newPass = newPass + random.choice(allChar)
+
+print(newPass)
