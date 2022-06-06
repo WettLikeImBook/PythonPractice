7e6efbc13baa3ea33aff5a9c0f40eee2b2ed4fc6
gyyost <yostgrant@icloud.com>
2022-06-06 17:45:21 -0400

    string

---
+++ picnicTable.py
@@ -0,0 +1,4 @@
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.')) + str(v).rjust(rightWidth)
